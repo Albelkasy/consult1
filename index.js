367c356c5adc26784cd3bf4e6f9911edc4427074
AliAlbelkasy <109920450+Albelkasy@users.noreply.github.com>
2022-08-15 16:20:26 +0200

    potoes

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true, useUnifiedTopolo
     console.log("Connected to MongoDB");
   }
 );
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use("/images", express.static(path.join(__dirname, "public")));
 
 //middleware
 app.use(express.json());
@@ -38,14 +38,14 @@ app.use(morgan("common"));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images");
+    cb(null, "public");
   },
   filename: (req, file, cb) => {
     cb(null,Date.now()+"-"+Math.random()*100+file.originalname);
   },
 });
 
-const upload = multer({ dest:"public/images", storage });
+const upload = multer({ dest:"public", storage });
 app.post("/api/upload/:id", upload.single("file"),async (req, res) => {
   try {
     console.log(req.file.path)
