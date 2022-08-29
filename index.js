7caba79cf9d20d839244be983c7cae486c7cfba1
AliAlbelkasy <109920450+Albelkasy@users.noreply.github.com>
2022-08-29 20:16:52 +0200

    vo

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true, useUnifiedTopolo
     console.log("Connected to MongoDB");
   }
 );
-app.use("public/images", express.static(path.join(__dirname, "public/images")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use( express.static(path.join(__dirname, "public")));
 
 //middleware
