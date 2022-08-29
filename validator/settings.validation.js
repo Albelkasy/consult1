023118d46bc696e314b50311634c076925c27e7e
AliAlbelkasy <109920450+Albelkasy@users.noreply.github.com>
2022-08-29 18:03:18 +0200

    h

--- validator/settings.validation.js
+++ validator/settings.validation.js
@@ -2,7 +2,7 @@ const{check}=require('express-validator')
 module.exports=[
     check('password').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$/),
     check('repass').custom((value,{req})=>{
-        if(value!==req.body.newpass){
+        if(value!==req.body.password){
             return false
         }
         return true
