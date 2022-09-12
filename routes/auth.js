71b290332380bfba75f69d50655c94c6f2021951
AliAlbelkasy <109920450+Albelkasy@users.noreply.github.com>
2022-09-12 11:24:50 +0200

    p

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const GOOGLE_CLIENT_SECRET = "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";
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://oauth2.example.com/auth?error=access_denied"
+    callbackURL: "/auth/google"
   },
   function(accessToken, refreshToken, profile, done) {
       userProfile=profile;
