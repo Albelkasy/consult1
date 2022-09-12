388e50b5245cbcc17882156a64b54b72a56f34e0
AliAlbelkasy <109920450+Albelkasy@users.noreply.github.com>
2022-09-12 20:53:04 +0200

    u

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 const app = require('express').Router();
 const passport = require('passport');
-const GOOGLE_CLIENT_ID = "337840481002-kh0vo87sapvimgs1l8upk81qjb3isbb6.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET="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";
+const GOOGLE_CLIENT_ID = "337840481002-6r65l3p55j070beocuol2nhtvi8cb342.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET="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";
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: "https://consultant1.herokuapp.com/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
