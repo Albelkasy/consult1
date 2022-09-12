c696fd6648f968b75fdc0c929c991a7bf1ebe0c7
AliAlbelkasy <109920450+Albelkasy@users.noreply.github.com>
2022-09-12 11:17:33 +0200

    b

--- routes/auth.js
+++ routes/auth.js
@@ -18,11 +18,11 @@ passport.use(new GoogleStrategy({
 app.get('/auth/google', 
   passport.authenticate('google', { scope : ['profile', 'email'] }));
  
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/error' }),
-  function(req, res) {
-    // Successful authentication, redirect success.
-    res.redirect('/success');
-  });
+// app.get('/auth/google/callback', 
+//   passport.authenticate('google', { failureRedirect: '/error' }),
+//   function(req, res) {
+//     // Successful authentication, redirect success.
+//     res.redirect('/success');
+//   });
 
 module.exports = app
