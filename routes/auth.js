7daaaffc93a4ce463d11345fe4a463aff327f907
AliAlbelkasy <109920450+Albelkasy@users.noreply.github.com>
2022-09-12 13:47:13 +0200

    g

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://consultant1.herokuapp.com/auth/google/callback",
+    callbackURL: "http://localhost:5000/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
